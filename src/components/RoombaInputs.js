c35692e3c980820b2829da67b00366732fc59f7f
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-22 19:45:48 +0100

    changed strings for headers

--- src/components/RoombaInputs.js
+++ src/components/RoombaInputs.js
@@ -1,34 +1,48 @@
 import React from 'react';
+import HooverLogo from '../images/HooverLogo.png';
 
 const RoombaInputs = (props) => {
     return (
         <div>
-            <h3>How to play</h3>
-            <div className='howToPlay'>
-                <h5>1. Fill in the inputs below</h5>
-                <h5>2. Click the submit button</h5>
-                <h5>3. A grid will appear with north/east/south/west buttons to move Roomba!</h5>
-                <h5>4. Use the buttons to navigate Roomba into the dirt tile you placed</h5>
-                <h5>5. Bear in mind that Roomba's position is symbolised by a red outline around the tile it's currently on</h5>
+            <div className="alignPageContent">
+                <div className="inline">
+                    <h3>How to play</h3>
+                    <div className='howToPlay'>
+                        <h5>1. Create your game below</h5>
+                        <h5>2. Click the Hoover logo when you are ready to play</h5>
+                        <h5>3. Grid will appear with direction buttons above to move Roomba</h5>
+                        <h5>4. Navigate Roomba into the dirt tile you placed</h5>
+                    </div>
+                </div>
+                <div className="inline">
+                    <form onSubmit={props.onSubmit} id="roombaForm">
+                        <h3>Room Dimensions</h3>
+                        <div className='numberInput'>
+                            <input type="number" name="width" min="1" placeholder="Grid width"
+                                   onChange={props.onChange} required/>
+                            <input type="number" name="length" min="1" placeholder="Grid length"
+                                   onChange={props.onChange} required/>
+                        </div>
+                        <h3>Dirt Patch</h3>
+                        <div className='numberInput'>
+                            <input type="number" name="x" min="0" max={props.grid.width - 1}
+                                   placeholder="Dirt x value" onChange={props.onChange}/>
+                            <input type="number" name="y" min="0" max={props.grid.length - 1}
+                                   placeholder="Dirt y value" onChange={props.onChange}/>
+                        </div>
+                        <h3>Roomba Start Tile</h3>
+                        <div className='numberInput'>
+                            <input type="number" name="x" min="0" max={props.grid.width - 1}
+                                   placeholder="Roomba x value" onChange={props.onChange} required/>
+                            <input type="number" name="y" min="0" max={props.grid.length - 1}
+                                   placeholder="Roomba y value" onChange={props.onChange} required/>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <form onSubmit={props.onSubmit}>
-                <h3>Room Dimensions</h3>
-                <div className='numberInput'>
-                    <input type="number" name="width" min="1" placeholder="Enter grid width" onChange={props.onChange} required/>
-                    <input type="number" name="length" min="1" placeholder="Enter grid length" onChange={props.onChange} required/>
-                </div>
-                <h3>Dirt Patch (x, y)</h3>
-                <div className='numberInput'>
-                    <input type="number" name="x" min="0" max={props.grid.width - 1} placeholder="Enter dirt x value" onChange={props.onChange}/>
-                    <input type="number" name="y" min="0" max={props.grid.length - 1} placeholder="Enter dirt y value" onChange={props.onChange}/>
-                </div>
-                <h3>Roomba Start Tile (x, y)</h3>
-                <div className='numberInput'>
-                    <input type="number" name="x" min="0" max={props.grid.width - 1} placeholder="Enter roomba x value" onChange={props.onChange} required/>
-                    <input type="number" name="y" min="0" max={props.grid.length - 1} placeholder="Enter roomba y value" onChange={props.onChange} required/>
-                </div>
-                <input className='submitButton' type="submit" value="Submit values"/>
-            </form>
+            <button className='submitButton' type='submit' form="roombaForm">
+                <img className='submitButtonImage' src={HooverLogo} alt="Play game"/>
+            </button>
         </div>
     );
 
